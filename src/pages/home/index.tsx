a6546f25f9661b980118d7199a8693ba2b3a74fd
erikarosah <erikarosamg@gmail.com>
2023-12-20 18:56:39 -0300

    home initial style

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,38 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
 import { Header } from '../../components/header';
-import { Container } from './style';
+import { Container, Content, Image } from './style';
 
 export function Home() {
   return (
     <Container>
       <Header />
+      <h2>Em alta</h2>
+      <Content>
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={50}
+          slidesPerView={1}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <Image
+              src="https://occ-0-2401-1123.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXDtAHEsLzQMsGdJWt7MITtyjk0JsnMxRFCwNp7jb50jXF6cqkzFtNPg_O8XcXb3q4U_vgYPSk5PrG_V0zq9tB71vgA22_QDETcyt_dHgR280ANI9sxAVDnXeam0feNRUyUS7R96vaWLM40PDG7buKoRNwuR0DZhogA3XynsAhQwYE77e5YbZuiofMaFd6OVKDm6GpFuWtzxSaWfSApB_PnqnnOmqYB3Q0Nn8SxyUwTOie8QOaVYL18R_ZRA35BJ4mUoloJnFqWuf_DMPR679qP_QhswI3fVRQNIhjioF4QCYVRUM96YmddMKAOEzULJSQ3euTQ6M0GMxLWAuZRjX-QTFrU6pa1CZJuR9lJEMD4.jpg?r=4f1"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="https://occ-0-2401-1123.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXDtAHEsLzQMsGdJWt7MITtyjk0JsnMxRFCwNp7jb50jXF6cqkzFtNPg_O8XcXb3q4U_vgYPSk5PrG_V0zq9tB71vgA22_QDETcyt_dHgR280ANI9sxAVDnXeam0feNRUyUS7R96vaWLM40PDG7buKoRNwuR0DZhogA3XynsAhQwYE77e5YbZuiofMaFd6OVKDm6GpFuWtzxSaWfSApB_PnqnnOmqYB3Q0Nn8SxyUwTOie8QOaVYL18R_ZRA35BJ4mUoloJnFqWuf_DMPR679qP_QhswI3fVRQNIhjioF4QCYVRUM96YmddMKAOEzULJSQ3euTQ6M0GMxLWAuZRjX-QTFrU6pa1CZJuR9lJEMD4.jpg?r=4f1"
+              alt=""
+            />
+          </SwiperSlide>
+        </Swiper>
+      </Content>
     </Container>
   );
 }
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,7 +1,24 @@
 import styled from 'styled-components';
 
+import 'swiper/css';
+
 export const Container = styled.div`
-  width: 100%;
-  height: 80vh;
-  background-color: red;
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    padding: 2rem 0 0 2rem;
+    font-size: 2rem;
+  }
+`;
+
+export const Content = styled.div`
+  height: 100vh;
+  display: flex;
+  padding: 2rem;
+`;
+
+export const Image = styled.img`
+  width: 23rem;
+  height: 13rem;
 `;
