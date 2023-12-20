f3d1ceb32140cc9470c0127a261ffc2c714d5317
erikarosah <erikarosamg@gmail.com>
2023-12-20 16:41:31 -0300

    initial styles

---
+++ src/styles/Global.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        font-size: 62.5%;
+        --bg-color: #121212;
+        --text-color: #f2f2f2;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Open-Sans, 'Poppins', sans-serif;
+        list-style: none;
+        text-decoration: none;
+        scroll-behavior: smooth;
+    }
+
+    body {
+        background: var(--bg-color);
+        color: var(--text-color);
+    } 
+`;
